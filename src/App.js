499ae089707271675ba39ff322293a9633ec7f88
Gabriel Almeidia <gabriel.alvikar@gmail.com>
2021-02-24 01:12:27 -0300

    Lista

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "./components/SearchBox";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList([
+      { title: "Comprar o bolo", done: false },
+      { title: "pegar o cachorro no Petshop", done: true },
+      { title: "Gravar aula", done: false },
+    ]);
+  }, []);
 
   function handleSearchInput(newText) {
     setSearchText(newText);
@@ -11,10 +20,20 @@ function App() {
   return (
     <>
       <h1>Lista de Tarefas</h1>
+
       <SearchBox onChangeText={handleSearchInput} />
       <SearchBox text={searchText} />
+
       <hr />
-      Texto procurado: {searchText}
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item.done && <del>{item.title}</del>}
+            {!item.done && item.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
