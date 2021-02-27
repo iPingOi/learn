29a4943c42c2d4ef1ae811afb72d091adc8144d8
Gabriel Almeidia <gabriel.alvikar@gmail.com>
2021-02-27 17:56:50 -0300

    LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 import React, { useState, useEffect } from "react";
-import SearchBox from "./components/SearchBox";
 
 function App() {
-  const [searchText, setSearchText] = useState("");
-  const [list, setList] = useState([]);
+  const [name, setName] = useState(localStorage.getItem("name"));
 
   useEffect(() => {
-    setList([
-      { title: "Comprar o bolo", done: false },
-      { title: "pegar o cachorro no Petshop", done: true },
-      { title: "Gravar aula", done: false },
-    ]);
-  }, []);
-
-  function addAction(newItem) {
-    let newList = [...list, { title: newItem, done: false }];
-
-    setList(newList);
-  }
-
-  function handleToggleDone(index) {
-    let newList = [...list];
-
-    newList[index].done = !newList[index].done;
-
-    setList(newList);
-  }
+    localStorage.setItem("name", name);
+  }, [name]);
 
   return (
     <>
-      <h1>Lista de Tarefas</h1>
-
-      <SearchBox padrao="Adicione um item" onEnter={addAction} />
-
-      <hr />
-
-      <ul>
-        {list.map((item, index) => (
-          <li key={index} onClick={() => handleToggleDone(index)}>
-            {item.done && <del>{item.title}</del>}
-            {!item.done && item.title}
-          </li>
-        ))}
-      </ul>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </>
   );
 }
