9aeeeec8a2b98b5a079033969d03fc5897f39bb5
Gabriel Almeidia <gabriel.alvikar@gmail.com>
2021-02-28 16:46:08 -0300

    Connect e Dispatch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
 
-function Home() {
+function Home(props) {
   let history = useHistory();
 
   const handleButton = () => {
@@ -9,12 +10,48 @@ function Home() {
     }, 2000);
   };
 
+  const handleGabriel = () => {
+    props.setName("Gabriel");
+  };
+
+  const handleIncrement = () => {
+    props.increment();
+  };
+
   return (
     <>
       <h1>Home</h1>
+      NOME: {props.name} <br />
+      Contagem: {props.contador} <br />
+      <br />
+      <button onClick={handleGabriel}>Setar nome para Gabriel</button>
+      <br />
+      <button onClick={handleIncrement}>+1</button>
+      <br />
       <button onClick={handleButton}>Ir para a página SOBRE</button>
     </>
   );
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    name: state.usuario.name,
+    contador: state.usuario.contador,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setName: (newName) =>
+      dispatch({
+        type: "SET_NAME",
+        payload: { name: newName },
+      }),
+    increment: () =>
+      dispatch({
+        type: "INCREMENT_CONTADOR",
+      }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
